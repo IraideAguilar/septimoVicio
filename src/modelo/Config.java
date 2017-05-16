5f1760c26332f30944bd6816e48cb56c8a88b2eb
ikaslea <ikaslea@pipi>
2017-05-16 12:55:58 +0200

    hay que hacer la ficha en el htm y jsp

---
+++ src/modelo/Config.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public interface Config {
+	
+	public static final String BBDD = "septimovicio";
+	public static final String HOST = "localhost";
+	public static final String USERNAME = "root";
+	public static final String PASSWORD = "";
+	
+
+}
